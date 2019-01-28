f345e6aed7c3477b442707b5a708ebb1fb2216a5
Shane M Mathews <shane.m.mathews@gmail.com>
2019-01-27 19:37:03 -0500

    add describe

--- timing_demo.c
+++ timing_demo.c
@@ -31,8 +31,33 @@ roaring_bitmap_t* shuffle_in_twitter(uint32_t numToSet) {
     }
   }
 
-  roaring_bitmap_run_optimize(r1);
   return r1;
+}
+
+void save(roaring_bitmap_t* r, char* filename) {
+  roaring_bitmap_run_optimize(r);
+  uint32_t expectedsize = roaring_bitmap_portable_size_in_bytes(r);
+  char *serializedbytes = malloc(expectedsize);
+  roaring_bitmap_portable_serialize(r, serializedbytes);
+  FILE *write_ptr = fopen(filename,"wb");  // w for write, b for binary
+  fwrite(serializedbytes,expectedsize,1,write_ptr); // write 10 bytes from our buffer
+  fclose(write_ptr);
+  free(serializedbytes);
+}
+
+roaring_bitmap_t* load(char* filename) {
+  FILE *fileptr = fopen(filename,"rb");  // r for read, b for binary
+  fseek(fileptr, 0, SEEK_END);          // Jump to the end of the file
+  long filelen = ftell(fileptr);             // Get the current byte offset in the file
+  rewind(fileptr);                      // Jump back to the beginning of the file
+
+  char* buffer = (char *)malloc((filelen+1)*sizeof(char)); // Enough memory for file + \0
+  fread(buffer, filelen, 1, fileptr); // Read in the entire file
+  fclose(fileptr); // Close the file
+
+  roaring_bitmap_t *loaded = roaring_bitmap_portable_deserialize(buffer);
+  free(buffer);
+  return loaded;
 }
 
 int main(int argc, char **argv) {
@@ -43,32 +68,18 @@ int main(int argc, char **argv) {
       printf("making bitset with %d set bits inside a total set of %d bits\n", values, total_twitter);
       roaring_bitmap_t *created = shuffle_in_twitter(values);
 
-      uint32_t expectedsize = roaring_bitmap_portable_size_in_bytes(created);
-      char *serializedbytes = malloc(expectedsize);
-      roaring_bitmap_portable_serialize(created, serializedbytes);
-      roaring_bitmap_free(created);
-
       char* outName = "output.roar";
       if(argc >= 4) {
         outName = argv[3];
       }
-      FILE *write_ptr = fopen(outName,"wb");  // w for write, b for binary
-      fwrite(serializedbytes,expectedsize,1,write_ptr); // write 10 bytes from our buffer
-      fclose(write_ptr);
-      free(serializedbytes);
-    } else if(strcmp(argv[1],"load") == 0 && argc >= 3) {
+      save(created,outName);
+      roaring_bitmap_free(created);
+    } else if(strcmp(argv[1],"describe") == 0 && argc >= 3) {
       char* inName = argv[2];
-      FILE *fileptr = fopen(inName,"rb");  // w for write, b for binary
-      fseek(fileptr, 0, SEEK_END);          // Jump to the end of the file
-      long filelen = ftell(fileptr);             // Get the current byte offset in the file
-      rewind(fileptr);                      // Jump back to the beginning of the file
 
-      char* buffer = (char *)malloc((filelen+1)*sizeof(char)); // Enough memory for file + \0
-      fread(buffer, filelen, 1, fileptr); // Read in the entire file
-      fclose(fileptr); // Close the file
+      roaring_bitmap_t *loaded = load(inName);
 
-      roaring_bitmap_t *loaded = roaring_bitmap_portable_deserialize(buffer);
-      free(buffer);
+      roaring_bitmap_printf_describe(loaded);
 
       roaring_bitmap_free(loaded);
     }
